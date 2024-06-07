bb92c55319bd63855f2eeb2a55554c65531ebe40
varun <aitvarunbhatia@gmail.com>
2024-06-07 16:53:38 +0530

    updated code

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello World\n");
+  const log = `${Date.now()}: new request received. ${req.url} \n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home Page\n");
+        break;
+      case "/about":
+        res.end("About Page\n");
+        break;
+      default:
+        res.end("404 Not Found\n");
+    }
+  });
 });
 
 server.listen(8080, () => {
