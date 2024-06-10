70466178b981d68f0a921380231aaa30249efe48
varun <aitvarunbhatia@gmail.com>
2024-06-10 09:56:22 +0530

    added express

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
 const http = require("http");
-const fs = require("fs");
+const express = require("express");
 const url = require("url");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end(); // ignore favicon requests
-  const log = `${Date.now()}: new request received. ${req.url} \n`;
-  const newUrl = url.parse(req.url, true);
-  console.log("", newUrl);
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (newUrl.pathname) {
-      case "/":
-        if (req.method === "GET") res.end("Home Page\n");
-        break;
-      case "/about/":
-        const userName = newUrl.query.name;
-        res.end(`hello ${userName}\n`);
-        break;
-      case "/signup":
-        if (req.method === "GET") {
-          res.end("Signup Page\n");
-        } else if (req.method === "POST") {
-          // DB insert
-          res.end("Signup successful\n");
-        }
-        break;
-      default:
-        res.end("404 Not Found\n");
-    }
-  });
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Installed Express HomePage!");
 });
 
-server.listen(8080, () => {
+app.listen(8080, () => {
   console.log("Server running at http://localhost:8080/");
 });
